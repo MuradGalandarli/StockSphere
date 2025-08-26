dd95be979d9bc678fb68eec3adf4460c53944127
Murad5455 <118770310+Murad5455@users.noreply.github.com>
2025-08-26 13:49:39 +0400

    Fix warehouse deletion to include stocks, products and categories

--- Infrastructure/StockSphere.Persistence/Services/WarehouseService.cs
+++ Infrastructure/StockSphere.Persistence/Services/WarehouseService.cs
@@ -1,7 +1,9 @@
-﻿using StockSphere.Application.Abstractions.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using StockSphere.Application.Abstractions.Services;
 using StockSphere.Application.Dtos;
 using StockSphere.Application.Repositories;
 using StockSphere.Domain.Entities;
+using StockSphere.Persistence.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,26 +15,24 @@ namespace StockSphere.Persistence.Services
 {
     public class WarehouseService : IWarehouseService
     {
-        private readonly IWarehouseWriteRepository _warehouseWriteRepository;
-        private readonly IWarehouseReadRepository _warehouseReadRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public WarehouseService(IWarehouseWriteRepository warehouseWriteRepository, IWarehouseReadRepository warehouseReadRepository)
+        public WarehouseService(IUnitOfWork unitOfWork)
         {
-            _warehouseWriteRepository = warehouseWriteRepository;
-            _warehouseReadRepository = warehouseReadRepository;
+            _unitOfWork = unitOfWork;
         }
 
         public async Task<bool> AddWarehouse(string name, string location)
         {
-           bool status = await _warehouseWriteRepository.AddAsync(new() { Location = location, Name = name });
-           await _warehouseWriteRepository.SaveAsync();
-           return status;
+            bool status = await _unitOfWork.WarehouseWriteRepository.AddAsync(new() { Location = location, Name = name });
+            await _unitOfWork.WarehouseWriteRepository.SaveAsync();
+            return status;
         }
 
         public List<WarehouseDto> GetAllWarehouse(int page, int size)
         {
-            var result = _warehouseReadRepository.GetAll().Skip((page-1)*size).Take(size);
-           return result.Select(w => new WarehouseDto()
+            var result = _unitOfWork.WarehouseReadRepository.GetAll().Skip((page - 1) * size).Take(size);
+            return result.Select(w => new WarehouseDto()
             {
                 Location = w.Location,
                 Name = w.Name,
@@ -43,30 +43,58 @@ namespace StockSphere.Persistence.Services
 
         public async Task<WarehouseDto> GetByIdWarehouse(int id)
         {
-           Warehouse warehouse = await _warehouseReadRepository.GetByIdAsync(id);
-            if(warehouse == null)
+            Warehouse warehouse = await _unitOfWork.WarehouseReadRepository.GetByIdAsync(id);
+            if (warehouse == null)
             {
                 return new();
             }
-            return new WarehouseDto() {Location = warehouse.Location, Name = warehouse.Name,Id = warehouse.Id};
+            return new WarehouseDto() { Location = warehouse.Location, Name = warehouse.Name, Id = warehouse.Id };
         }
 
         public async Task<bool> RemoveWarehouse(int id)
         {
-            Warehouse warehouse = await _warehouseReadRepository.GetByIdAsync(id);
-            bool result = _warehouseWriteRepository.Delete(warehouse);
-            await _warehouseWriteRepository.SaveAsync();
-            return result;
+            Warehouse? warehouse = await _unitOfWork.WarehouseReadRepository.GetWhere(w => w.Id == id).Include(s => s.Stocks).
+                ThenInclude(p => p.Product).ThenInclude(c => c.Category).FirstOrDefaultAsync();
+            if (warehouse == null)
+                return false;
+
+            using var transaction = await _unitOfWork.BeginTransactionAsync();
+            try
+            {
+                _unitOfWork.WarehouseWriteRepository.Delete(warehouse);
+
+                if (warehouse.Stocks.Count > 0)
+                {
+                    foreach (Stock stock in warehouse.Stocks)
+                    {
+                        _unitOfWork.WarehouseWriteRepository.Delete(warehouse);
+                        if (stock.Product != null)
+                            _unitOfWork.ProductWriteRepository.Delete(stock.Product);
+                        if (stock.Product.Category != null)
+                            _unitOfWork.CategoryWriteRepository.Delete(stock.Product.Category);
+                    }
+                }
+
+                await transaction.CommitAsync();
+                await _unitOfWork.WarehouseWriteRepository.SaveAsync();
+                return true;
+            }
+            catch
+            {
+                await _unitOfWork.RollbackAsync(transaction);
+                return false;
+            }
+
         }
 
         public async Task<bool> WarehouseUpdate(Warehouse warehouse)
         {
-           var result = await _warehouseReadRepository.GetSingleAsync(x => x.Id == warehouse.Id);
+            var result = await _unitOfWork.WarehouseReadRepository.GetSingleAsync(x => x.Id == warehouse.Id);
             if (result != null)
             {
                 result.Name = warehouse.Name;
                 result.Location = warehouse.Location;
-                await _warehouseWriteRepository.SaveAsync();
+                await _unitOfWork.WarehouseWriteRepository.SaveAsync();
                 return true;
             }
             return false;
